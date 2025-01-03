c96a4ce16b37b684d97462e6bc5c299c103f25a7
B.Deekshitha <128179619+deekshithabakanagari@users.noreply.github.com>
2025-01-03 17:38:16 +0530

    Add files via upload

---
+++ nested loop.py
@@ -0,0 +1,34 @@
+# for i in range(1,13):
+#     for j in range(1,10):
+#         print(i,j)
+
+
+# while loop
+# i = 5
+# while(i>0):              -->reverse
+#     print(i)
+#     i-=1
+
+# for i in range(7,0,-1):    -->reverse
+#     print(i)
+
+
+# for i in reversed(range(11)):
+#      print(i)
+
+#pairs in nested loop
+#    1,1    1,2   1,3 
+#    2,1    2,2   2,3
+#    3,1    3,2   2,3
+
+
+#keywords = pass,continue,break 
+# for i in range(1,8):
+#     if i == 3:
+#         continue      #returns 1,2 
+#     elif i == 6:
+#       break        #does'nt return 6 and 7 also
+#     print(i)
+
+
+
