f42c507e011ccfae059018056d9b64d3159a144a
B.Deekshitha <128179619+deekshithabakanagari@users.noreply.github.com>
2025-02-06 09:29:20 +0530

    Add files via upload

---
+++ codes.py
@@ -0,0 +1,23 @@
+Inorder to ensure
+one of the games
+Alexa is amazons virtual assistant
+The team of machine learning scientists
+the developersa at amazon employ
+Aws provides scalable system
+amazon web services stores greyscale images
+amazon academy has launched new course on quantum physucs
+Data scientists at amazon are working on cleaning a machine
+min distance
+amazon prime day
+splitprefixSuffix
+amazon uses small roomba shaped robotd called drives
+
+
+n = int(input())
+fact = 1
+i = 1
+while i <= n:
+    fact = fact*i
+    i+=1
+print(fact)
+    
