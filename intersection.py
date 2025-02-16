ba7f614026111fd02c51b4d6c0ece23635549245
B.Deekshitha <128179619+deekshithabakanagari@users.noreply.github.com>
2025-02-16 13:08:12 +0530

    Add files via upload

--- intersection.py
+++ intersection.py
@@ -1,25 +1,18 @@
-def intersection(l1, l2):
-    hashmap = dict()
-    for i in l1:
-        if i in hashmap:
-            hashmap[i] += 1
-        else:
-            hashmap[i] = 1
+# This code takes three sets as input
+# Prints the common elements of the three sets
 
-    result = []
-    for i in l2:
-        if i in hashmap and hashmap[i]!=0:
-            result.append(i)
-            hashmap[i]-= 1
-    return result
+# Taking input from the user
+set_1 = input('Enter comma seperated numbers: ').split()
+set_2 = input('Enter comma seperated numbers: ').split()
+set_3 = input('Enter comma seperated numbers: ').split()
 
-#intersection
-def intersection(l1,l2):
-    result =[]
-    for i in l2:
-        if i in l1:
-            result.append(i)
-    return result
-l1=[1, 2, 3, 4, 5, 6]
-l2=[2, 4, 6, 8, 10, 12]
-print(intersection(l1,l2))
+# Checking for the common elements
+temp = set(set_1) & set(set_2)
+result = list(set(set_3) & temp)
+
+# Converting the result elements to ints
+for i in range(len(result)):
+    result[i] = int(result[i])
+
+# Printing the output
+print(result)
