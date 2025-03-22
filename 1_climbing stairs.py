c8bd652f9f0fcef0097d62c2c8cbcb99717365bc
B.Deekshitha <128179619+deekshithabakanagari@users.noreply.github.com>
2025-03-22 13:13:31 +0530

    Add files via upload

---
+++ 1_climbing stairs.py
@@ -0,0 +1,10 @@
+#shortcuts to recursion and Dp and memo
+
+#1.Try to represent the problem in terms of index
+
+#2. do all possible stuffs on that index according to problem statement
+
+#3. sum of all stuffs --> count all ways()
+    # min of all stuffs----> find min
+
+
