23b28bf8dfb2ba4f81b19d6449dffadaccbad029
B.Deekshitha <128179619+deekshithabakanagari@users.noreply.github.com>
2025-09-14 10:35:53 +0530

    Add files via upload

---
+++ 8_Max_subarray.py
@@ -0,0 +1,8 @@
+# This code is used to find the max subarray sum in an array
+# Did not complete
+
+def max_subarray(nums):
+    pass
+
+nums = list(map(int, input('Enter space separated nums: ').split()))
+print(max_subarray(nums))
